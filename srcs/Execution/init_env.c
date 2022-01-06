296472b48d33ecc4f96b6ccddd466b94694a7e00
Medaz-Sploit <azoukni_mohamed@protonmail.com>
2022-01-06 01:42:47 +0100

    Done

--- srcs/Execution/init_env.c
+++ srcs/Execution/init_env.c
@@ -3,20 +3,33 @@
 void init_env(t_parser *parser, char **envp)
 {
 	t_env *env;
-	t_list *tmp;
-	char **env;
+	t_list *tmp_list;
+	char **env_table;
 	int i;
 	
-	i = 0;
+	i = 1;
 	
 	env = ft_calloc(1, sizeof(t_env));
 	if (!env)
 		exit_error(2);
-	env = ft_split(envp[0], "=");
-	tmp = 
+	env_table = ft_split(envp[0], '=');
+	env->name = ft_strdup(env_table[0]);
+	env->content = ft_strdup(env_table[1]);
+	tmp_list = ft_lstnew(env_table);
+	printf("env_table name: %s\tcontent: %s\n", env->name, env->content);
+	free(env_table);
 	while (envp[i])
 	{
-		printf ("%s\n", envp[i]);
+		env_table = ft_split(envp[i], '=');
+		env = ft_calloc(1, sizeof(t_env));
+		if (!env)
+			exit_error(2);
+		env->name = ft_strdup(env_table[0]);
+		env->content = ft_strdup(env_table[1]);
+		ft_lstadd_back(&(tmp_list), ft_lstnew(env));
+		printf("%s=%s\n", env->name, env->content);
+		free(env_table);
 		i++;
 	}
+	parser->env = tmp_list;
 }
--- srcs/Parser/error.c
+++ srcs/Parser/error.c
@@ -6,7 +6,7 @@
 /*   By: mazoukni <mazoukni@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 11:01:44 by mazoukni          #+#    #+#             */
-/*   Updated: 2022/01/05 11:20:01 by mazoukni         ###   ########.fr       */
+/*   Updated: 2022/01/05 22:34:40 by mazoukni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int exit_error(int e)
 {
 	if (e == 0)
 		printf("Error:\n\t Exit Successfuly\n");
-	else if (e < 1 && e > 10)
+	else if (e > 2 && e < 10)
 		printf("Error:\n\t Malloc FAILED\n");
 	else
 		printf("Error\n");
